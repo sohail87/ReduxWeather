8dca395b247c1c5c5daa6377560376c6af2e10a2
sohail87 <sohaildeen73@gmail.com>
2017-06-03 18:51:40 +0100

    prevent form auto submit

--- src/containers/search_bar.js
+++ src/containers/search_bar.js
@@ -4,16 +4,25 @@ export default class SearchBar extends Component {
   constructor(props){
     super(props);
     this.state = {term:''};
+    //if youve got a callback that makes a reference to this
+    //you need to bind it
     this.onInputChange = this.onInputChange.bind(this);//bind the context to this Component
   }
   onInputChange(event){
     console.log(event.target.value);
     this.setState({term: event.target.value})
   }
+  onFormSubmit(event){
+    event.preventDefault(); //stop auto submit when a button clicked or enter is hit on keyboard
+
+    //we need to go a fetch weather data here
+    //
+  }
   render() {
     return (
-      <form className="input-group">
-        <input placeholder="get a five day forecast in your favourite cities"
+      <form className="input-group" onSubmit={this.onFormSubmit}>
+        <input
+          placeholder="get a five day forecast in your favourite cities"
           className="form-control"
           value={this.state.term}
           onChange={this.onInputChange}
